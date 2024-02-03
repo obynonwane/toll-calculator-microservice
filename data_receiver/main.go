c730fd891f1cd7d62b579e3aac2427c0f61395f4
obynonwane <biostechnologyng@gmail.com>
2024-02-03 18:01:27 +0100

    update

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -9,10 +9,6 @@ import (
 	"github.com/obynonwane/tolling/types"
 )
 
-func main() {
-	fmt.Println("data receiver working fine")
-}
-
 // this is a struct of DataReceiver
 // with a msg field is a channel
 // designed to handle data of type OBUData
@@ -21,18 +17,38 @@ type DataReceiver struct {
 	conn  *websocket.Conn
 }
 
-// this function handles the ws data sent
+// create a new instance of the DataReceiver struct
+func NewDataReceiver() *DataReceiver {
+	return &DataReceiver{
+		msgch: make(chan types.OBUData, 128),
+	}
+}
+
+func main() {
+	recv := NewDataReceiver()
+	http.HandleFunc("/ws", recv.handlerWS) //websocket endpoint
+	http.ListenAndServe(":3000", nil)      //serve the server
+}
+
+// this function handles the websocket connection
 func (dr *DataReceiver) handlerWS(w http.ResponseWriter, r *http.Request) {
-	conn, err := websocket.Upgrade(w, r, nil, 1028, 1028)
+	u := websocket.Upgrader{
+		ReadBufferSize:  1028,
+		WriteBufferSize: 1028,
+	}
+	conn, err := u.Upgrade(w, r, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	dr.conn = conn
+
+	go dr.wsReceiveLoop()
 }
 
 // this func loops through the data
 func (dr *DataReceiver) wsReceiveLoop() {
+	fmt.Println("NEW OBU connected client connected!")
 	for {
 		var data types.OBUData
 		if err := dr.conn.ReadJSON(&data); err != nil {
@@ -40,6 +56,7 @@ func (dr *DataReceiver) wsReceiveLoop() {
 			continue
 		}
 
+		fmt.Printf("received OBU data from [%d] :: <lat %.2f, long $\n", data.OBUID)
 		//pipe the data into the message channel
 		dr.msgch <- data
 	}
