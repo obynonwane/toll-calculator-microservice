2899dbacbcda242823fe9d86e114bbdd3bbcb0e8
obynonwane <biostechnologyng@gmail.com>
2024-02-03 15:46:34 +0100

    update

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -17,13 +17,30 @@ func main() {
 // with a msg field is a channel
 // designed to handle data of type OBUData
 type DataReceiver struct {
-	msg  chan types.OBUData
-	conn *websocket.Conn
+	msgch chan types.OBUData
+	conn  *websocket.Conn
 }
 
-func (dr DataReceiver) handlerWS(w http.ResponseWriter, r *http.Request) {
+// this function handles the ws data sent
+func (dr *DataReceiver) handlerWS(w http.ResponseWriter, r *http.Request) {
 	conn, err := websocket.Upgrade(w, r, nil, 1028, 1028)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	dr.conn = conn
+}
+
+// this func loops through the data
+func (dr *DataReceiver) wsReceiveLoop() {
+	for {
+		var data types.OBUData
+		if err := dr.conn.ReadJSON(&data); err != nil {
+			log.Println("read error:", err)
+			continue
+		}
+
+		//pipe the data into the message channel
+		dr.msgch <- data
 	}
 }
