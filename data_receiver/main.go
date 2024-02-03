7f57740b0b67aafd15082f8eb83b1f12280785f4
obynonwane <biostechnologyng@gmail.com>
2024-02-03 14:57:53 +0100

    handling websocket data

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -17,10 +17,11 @@ func main() {
 // with a msg field is a channel
 // designed to handle data of type OBUData
 type DataReceiver struct {
-	msg chan types.OBUData
+	msg  chan types.OBUData
+	conn *websocket.Conn
 }
 
-func wsHandler(w http.ResponseWriter, r *http.Request) {
+func (dr DataReceiver) handlerWS(w http.ResponseWriter, r *http.Request) {
 	conn, err := websocket.Upgrade(w, r, nil, 1028, 1028)
 	if err != nil {
 		log.Fatal(err)
